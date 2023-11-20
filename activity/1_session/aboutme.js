ffc018142f6dfd6b9a2904593c2214023a91dd29
dhoneyands <davidhoneyands@gmail.com>
2023-11-20 20:54:01 +0100

    Activity 1 completion

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,12 @@
 console.log("hello world");
 
+//declaring variables
+let name = "David Honeyands Martí";
+let currentProfession = "Business Analyst";
+let desiredProfession = "Product Owner";
+
+//variable string
+let introduction = `Hola, me llamo ${name} y actualmente soy ${currentProfession}. Estoy cursando este Máster porque me gustaría trabajar como ${desiredProfession}`;
+console.log(introduction);
 
 console.log("laater");
