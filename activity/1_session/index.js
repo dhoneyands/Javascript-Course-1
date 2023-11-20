948db33daf763724e4831f6ddeb9ea849196728f
dhoneyands <davidhoneyands@gmail.com>
2023-11-20 21:42:25 +0100

    Introducing the solution to log any number multiple of 7

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -3,9 +3,11 @@ import "./aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2
+//No requiere de cambios ni importación de módulos
 
 // Ejercicio 3
-
+console.log("Ejercicio 3");
+import "./ejercicio3.js";
 // Ejercicio 4
 
 // Ejercicio 5
