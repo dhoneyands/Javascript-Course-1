c63fec7a672403e7a3417ae1ecfa64bdce5c9b9e
dhoneyands <davidhoneyands@gmail.com>
2023-11-22 19:08:22 +0100

    Solución al ejercicio 3

---
+++ activity/1_session/ejercicio3.js
@@ -0,0 +1,12 @@
+// Define the range of numbers you want to check
+const startNumber = 1;
+const endNumber = 100;
+
+// Loop through the numbers in the specified range
+for (let i = startNumber; i <= endNumber; i++) {
+  // Check the condition (e.g., if the number is even)
+  if (i % 7 === 0) {
+    // Log the number to the console
+    console.log(i);
+  }
+}
