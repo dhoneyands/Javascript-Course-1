07504d33399a4391cb2bdf58d5ed2b9ee5f6c8bf
dhoneyands <davidhoneyands@gmail.com>
2023-11-24 21:24:39 +0100

    Including the solution to the append function

---
+++ activity/1_session/ejercicio6.js
@@ -0,0 +1,9 @@
+console.log("Ejercicio 6");
+
+//Declaración del objeto
+let formatter = {
+    prefix:"Hello",
+    append: function(value){
+        let finalResult = `${prefix} ${value}`;
+    }
+}
